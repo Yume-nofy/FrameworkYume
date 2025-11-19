18cd6a9a7603eccb8bbeecb538b0b85ea8083264
yume-nofy <nofinainanormarakotondrasoa@gmail.com>
2025-11-19 13:29:35 +0300

    go,sprint4_bis

---
+++ sprint1_nofy/src/nofy/p17/ModelView.java
@@ -0,0 +1,13 @@
+package nofy.p17;
+public class ModelView{
+    private String view;
+    public ModelView(String view){
+        this.view=view;
+    }
+    public void setView(String view){
+        this.view=view;
+    }
+    public String getView(){
+        return this.view;
+    } 
+}
